a4fa55e0563a77cfaa9c57eb9af5d5f108581c8b
Timo's Design Backend <backend@timos.design>
2020-07-30 14:44:38 +0200

    fixed cors

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,7 @@ import { AppModule } from './app.module';
 config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: [
-      'http://localhost:8080',
-      'https://newsroom.timos.design',
-      'https://portfolio.timos.design',
-      'https://timos.design'
-    ],
-  });
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(80);
 }
 bootstrap();
