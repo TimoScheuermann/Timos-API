d7adb5a6853ae3709c689ae3e9b069de90c35a89
Timo Scheuermann <timo.scheuermann@gmx.de>
2020-08-02 14:10:32 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -5,23 +5,6 @@ import * as fs from 'fs-sync';
 import { AppModule } from './app.module';
 import { getEnv } from './config';
 
-async function bootstrap() {
-  if (getEnv('IS_LOCAL')) {
-    const app = await NestFactory.create(AppModule);
-    this.configureCors(app);
-    await app.listen(3000);
-  } else {
-    const certificate = fs.read(getEnv('pathToCert'));
-    const privateKey = fs.read(getEnv('pathToKey'));
-    const credentials = { key: privateKey, cert: certificate };
-    const app = await NestFactory.create(AppModule, {
-      httpsOptions: credentials,
-    });
-    this.configureCors(app);
-    await app.listen(3000);
-  }
-}
-
 function configureCors(app: INestApplication): void {
   const whitelist = [
     'https://accounts.timos.design',
@@ -43,6 +26,23 @@ function configureCors(app: INestApplication): void {
     },
   };
   app.use(cors(corsOptions));
+}
+
+async function bootstrap() {
+  if (getEnv('IS_LOCAL')) {
+    const app = await NestFactory.create(AppModule);
+    configureCors(app);
+    await app.listen(3000);
+  } else {
+    const certificate = fs.read(getEnv('pathToCert'));
+    const privateKey = fs.read(getEnv('pathToKey'));
+    const credentials = { key: privateKey, cert: certificate };
+    const app = await NestFactory.create(AppModule, {
+      httpsOptions: credentials,
+    });
+    configureCors(app);
+    await app.listen(443);
+  }
 }
 
 bootstrap();
